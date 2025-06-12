4692fe0a52c4e3bf2a20f40d3fce0285cbe8123e
Vu Tran Xuan <txvu20@gmail.com>
2025-06-12 15:43:28 +0700

    Update CI 2

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 def add(a, b):
     return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+def power(a, b):
+    return a ** b
+
+def modulus(a, b):
+    return a % b
--- test_main.py
+++ test_main.py
@@ -1,4 +1,7 @@
-from main import add
+from main import add, subtract
 
 def test_add():
-    assert add(1, 2) == 4
+    assert add(1, 2) == 3
+
+def test_subtract():
+    assert subtract(3, 1) == 2
