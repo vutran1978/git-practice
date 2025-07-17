9c58a70f9d9232c2a8e50a39ff760642511e2e1a
Victor Von Doom <victorvondoom@Trans-MacBook-Pro.local>
2025-07-17 15:26:42 +0700

    Add more functions

--- test_main.py
+++ test_main.py
@@ -1,7 +1,19 @@
-from main import add, subtract
+from main import add, subtract, multiply, divide, power, modulus
 
 def test_add():
     assert add(1, 2) == 3
 
 def test_subtract():
     assert subtract(3, 1) == 2
+
+def test_multiply():
+    assert multiply(2, 3) == 6
+
+def test_divide():
+    assert divide(6, 2) == 3
+
+def test_power():
+    assert power(2, 3) == 8
+
+def test_modulus():
+    assert modulus(10, 3) == 1
